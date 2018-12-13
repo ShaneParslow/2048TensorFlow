73018001281d03357b7a32dabd83a6e31183f7b6
Shane Parslow <shaneparslow808@gmail.com>
2018-12-12 20:16:17 -1000

    lost sanity, but random generation is in

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ class twenty48():
 		self.size = dimensions
 
 		#create empty board filled with zeroes of size provided
-		self.board = [[0] * self.size] * self.size
+		self.board = [[0 for i in range(self.size)] for j in range(self.size)]
+		self.addToBoard(2)
 
-	def addToBoard(num):#add numbers to board. 90% chance of 2, 10% chance of 4.
-		chosen = []#list of coordinates already chosen, so that they aren't picked again
-
+	#GAZE UPON THIS METHOD WITH FEAR, FOR IT IS MUCH MORE INTIMIDATING THAN IT LOOKS
+	#also I sacrificed my sanity to it
+	def addToBoard(self, num):#add numbers to board. 90% chance of 2, 10% chance of 4.
 		for i in range(num):
-			coord = np.random.randint(self.size + 1, size=2)#add 1 because numpy is non-inclusive
-			chosen.append(coord)
+			collision = True
 
-			value = 4 if np.random.rand() > 0.9 else 2#90% chance of tile with 2, 10% chance of 4
+			while collision:#keep repeating until coordinate is selected that isnt already chosen and is empty
+				x_coord = np.random.randint(self.size)
+				y_coord = np.random.randint(self.size)
+				value = 4 if np.random.rand() > 0.9 else 2#90% chance of tile with 2, 10% chance of 4
+
+				if self.board[y_coord][x_coord] == 0:
+					collision = False#good to select next coordinate
+					self.board[x_coord][y_coord] = value#add coord and value to board
 
 game = twenty48(4)
 print(np.matrix(game.board))
