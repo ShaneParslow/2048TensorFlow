78cfb55cb61889b7109ec7d90fe5bfd6406fed1b
Shane Parslow <shaneparslow808@gmail.com>
2018-12-12 18:04:48 -1000

    random generation of tiles

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ import tensorflow as tf
 import numpy as np
 
 class twenty48():
-    def __init__(self, size):
-        #create empty board filled with zeroes of size provided
-        self.board = [[0] * size] * size
+	def __init__(self, dimensions):
+		self.size = dimensions
 
-game = twenty48(3)
+		#create empty board filled with zeroes of size provided
+		self.board = [[0] * self.size] * self.size
+
+	def addToBoard(num):#add numbers to board. 90% chance of 2, 10% chance of 4.
+		chosen = []#list of coordinates already chosen, so that they aren't picked again
+
+		for i in range(num):
+			coord = np.random.randint(self.size + 1, size=2)#add 1 because numpy is non-inclusive
+			chosen.append(coord)
+
+			value = 4 if np.random.rand() > 0.9 else 2#90% chance of tile with 2, 10% chance of 4
+
+game = twenty48(4)
 print(np.matrix(game.board))
