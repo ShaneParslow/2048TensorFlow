f5da1d1fbad6b7abecd6edbc8b48e4b18e09fd3c
Shane Parslow <shaneparslow808@gmail.com>
2019-03-09 15:48:22 -1000

    Allow game to be played through terminal

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ import game
 
 
 t48 = game.Twenty48()
-print(np.matrix(t48.board))
-print(t48.score)
+while True:
+    print(np.matrix(t48.board))
+    print(t48.score)
+    move = input("Direction to move: ")
+    if move == "u":
+        t48.shift_up()
+    elif move == "d":
+        t48.shift_down()
+    elif move == "l":
+        t48.shift_left()
+    elif move == "r":
+        t48.shift_right()
+    else:
+        print("Invalid move")
