3017ec4088ffff35704fa476c2764a708bf039fe
Shane Parslow <shaneparslow808@gmail.com>
2019-03-10 00:20:58 -1000

    Spacing between labels

--- ui.py
+++ ui.py
@@ -4,7 +4,6 @@ def init_ui(size):
     global window
     global ui_board
     window = tk.Tk()
-    window.geometry("100x100")
 
     # Fill UI board with zeroes
     ui_board = [[tk.Label(window,text="0") for i in range(size)] for j in range(size)]
@@ -12,7 +11,7 @@ def init_ui(size):
     row_iterator = 0
     for row in ui_board:
         for label_iterator in range(0,len(row)):
-            ui_board[row_iterator][label_iterator].grid(row=row_iterator,column=label_iterator)
+            ui_board[row_iterator][label_iterator].grid(row=row_iterator,column=label_iterator,ipadx=10,ipady=10)
         row_iterator += 1
 
 def update_ui():
