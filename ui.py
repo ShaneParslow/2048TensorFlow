ceb9a3180cf633738c68df0cc6b89ee73fc21be4
Shane Parslow <shaneparslow808@gmail.com>
2019-03-09 23:59:28 -1000

    Small optimization of grid setup

--- ui.py
+++ ui.py
@@ -4,16 +4,15 @@ def init_ui(size):
     global window
     global ui_board
     window = tk.Tk()
+    window.geometry("100x100")
 
     # Fill UI board with zeroes
     ui_board = [[tk.Label(window,text="0") for i in range(size)] for j in range(size)]
     # Setup geometry managers for each label in ui_board
     row_iterator = 0
     for row in ui_board:
-        label_iterator = 0
-        for label in row:
+        for label_iterator in range(0,len(row)):
             ui_board[row_iterator][label_iterator].grid(row=row_iterator,column=label_iterator)
-            label_iterator += 1
         row_iterator += 1
 
 def update_ui():
