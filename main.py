296817ba2e69986f8056c7568fc693d57b182498
Shane Parslow <44428643+ShaneParslow@users.noreply.github.com>
2019-03-15 13:08:46 -1000

    New label when losing

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ import ui
 t48 = game.Twenty48()
 ui.init_ui(t48)
 
+print(np.matrix(t48.board))
 while True:
-    print(np.matrix(t48.board))
-    while True:
-        ui.update_ui(t48)
-        ui.window.update_idletasks()
-        ui.window.update()
+    ui.update_ui(t48)
+    ui.window.update_idletasks()
+    ui.window.update()
+    if t48.hasLost == True:
+        break
+# Final update to show most recent move
+ui.update_ui(t48)
+tk.Label(ui.window,text="You lost").grid(row=t48.size,column=1)
+# Make sure window doesn't freeze
+while True:
+    ui.window.update_idletasks()
+    ui.window.update()
