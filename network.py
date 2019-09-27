c06eff4456da1d4537c7559e4ca6dbb8934c9466
Shane Parslow <shaneparslow808@gmail.com>
2019-09-26 15:29:46 -1000

    revert to old network

--- network.py
+++ network.py
@@ -1,5 +1,3 @@
-# FUCK THIS FUCKING FUCK PIECE OF SHIT FUCK #
-
 import keras as ks
 from rl.agents.dqn import DQNAgent
 from rl.policy import EpsGreedyQPolicy
@@ -15,39 +13,16 @@ gym_env = t48_env.Twenty48Gym()
 def init_network():
     # Keras initial model and layers
     model = ks.Sequential()
-    model.add(ks.layers.Flatten(input_shape=(1,4,4)))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(100))
-    model.add(ks.layers.Activation('relu'))
-    model.add(ks.layers.Dense(4))
-    model.add(ks.layers.Activation('linear'))
+    model.add(ks.layers.Dense(100, input_shape=(1, 4, 4), activation="relu"))  # Hidden layer 1
+    model.add(ks.layers.Dense(100, activation="relu"))  # Hidden layer 2
+    model.add(ks.layers.Dense(100, activation="relu"))  # Hidden layer 3
+    model.add(ks.layers.Dense(100, activation="relu"))  # Hidden layer 4
+    model.add(ks.layers.Dense(100, activation="relu"))  # Hidden layer 5
+    model.add(ks.layers.Dense(100, activation="relu"))  # Hidden layer 6
+    model.add(ks.layers.Dense(100, activation="relu"))  # Hidden layer 7
+    model.add(ks.layers.Dense(1, activation="linear"))  # 1 list of 4 possible movements for output
+    model.add(ks.layers.Flatten())
+
     print(model.summary())
 
     memory = SequentialMemory(limit=50000, window_length=1)
@@ -59,7 +34,7 @@ def init_network():
 
 
 def train_network(agent):
-    agent.fit(gym_env, nb_steps=float(20000*30), visualize=True, nb_max_episode_steps=0)
+    agent.fit(gym_env, nb_steps=float(20000*35), visualize=True, nb_max_episode_steps=0)
     return gym_env
 
 
@@ -68,12 +43,11 @@ try:
     train_network(dqn_agent)
 except (KeyboardInterrupt, TclError):
     dqn_agent.save_weights('dqn_t48_weights.h5f', overwrite=True)
-    # print(len(range(1, len(gym_env.avg_scores))))
-    # print(gym_env.avg_scores)
+    # print(len(range(1, gym_env.num_instances)))
+    # print(len(gym_env.avg_scores))
     plt.scatter(range(1, len(gym_env.avg_scores) + 1), gym_env.avg_scores)
     plt.show()
     exit()
 dqn_agent.save_weights('dqn_t48_weights.h5f', overwrite=True)
 plt.scatter(range(1, len(gym_env.avg_scores) + 1), gym_env.avg_scores)
 plt.show()
-dqn_agent.test(gym_env, nb_episodes=5, visualize=True)
